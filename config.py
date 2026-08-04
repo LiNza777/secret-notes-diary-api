4442cb7094f2a9a3121ac89d9a7bf672abe49a50
liNza777 <olfun9@gmail.com>
2026-08-04 14:32:00 +0300

    async tests4

--- config.py
+++ config.py
@@ -7,6 +7,13 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_URL: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
 settings = Settings()
