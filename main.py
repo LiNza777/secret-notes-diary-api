a7ad6de55f09fd289471180aec3c3ed627d150fc
liNza777 <olfun9@gmail.com>
2026-08-05 16:45:48 +0300

    final async version 1.1.3

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from contextlib import asynccontextmanager
+from alembic.config import Config
+from alembic import command
 from fastapi import FastAPI
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
@@ -6,10 +9,27 @@ from limiter import limiter
 from routers_auth import auth_router
 from routers_notes import notes_router
 
+
+def run_migrations():
+    """Запуск миграций Alembic перед стартом сервера."""
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Запуск миграций Alembic...")
+    run_migrations()
+    print("Миграции успешно применены!")
+    yield
+
+
+
 app = FastAPI(
     title="Secret Notes API",
     description="API для безопасного дневника с JWT-авторизацией",
     version="1.0.0",
+    lifespan=lifespan,
 )
 
 app.state.limiter = limiter
