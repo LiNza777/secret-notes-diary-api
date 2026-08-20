42a470c7e62a5fdd893c9de50aba169c3fa2dcf4
liNza777 <olfun9@gmail.com>
2026-08-20 15:44:03 +0300

    add ai summary endpoint v.1.3

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str | None = None
     REDIS_PORT: int = 6379
     REDIS_PASSWORD: str | None = None
-
+    OPENROUTER_API_KEY: str
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
